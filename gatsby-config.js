f7dd6307b0b4d893c44185c78c0494598e100d7e
Tiago Martinho <tiago@elit.software>
2018-05-13 17:43:23 +0200

    Change site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Stellar by HTML5 UP',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js Starter based on Stellar by HTML5 UP',
+    title: "What's poppin?",
+    author: 'Crowders',
+    description: '',
   },
   pathPrefix: '/',
   plugins: [
