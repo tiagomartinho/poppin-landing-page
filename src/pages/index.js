57fdaafbf8002fe68b666c33941c57595fee7cd5
Tiago Martinho <tiago@elit.software>
2018-05-13 17:10:23 +0200

    Add Video section

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,6 +62,17 @@ class Index extends React.Component {
             </div>
           </section>
 
+          <section className="special">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/3jXFwnTovCk?rel=0&amp;showinfo=0"
+              frameborder="0"
+              allow="autoplay; encrypted-media"
+              allowfullscreen
+            />
+          </section>
+
           <section id="cta" className="main special">
             <header className="major">
               <h2>Interested?</h2>
